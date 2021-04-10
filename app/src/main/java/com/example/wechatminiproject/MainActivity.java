70dc2435ff5c84b3649926696289c7c90fcb7490
ArerehRaj <raj.jr@somaiya.edu>
2021-04-10 14:30:40 +0530

    SignUp Page Designed

--- app/src/main/java/com/example/wechatminiproject/MainActivity.java
+++ app/src/main/java/com/example/wechatminiproject/MainActivity.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 
 import com.parse.ParseInstallation;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
