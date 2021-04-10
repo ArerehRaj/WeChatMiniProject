067736497ca519e6ea925370f9f703132b70f445
ArerehRaj <raj.jr@somaiya.edu>
2021-04-10 15:34:46 +0530

    Authentication Done

--- app/src/main/java/com/example/wechatminiproject/LoginActivity.java
+++ app/src/main/java/com/example/wechatminiproject/LoginActivity.java
@@ -1,19 +1,92 @@
 package com.example.wechatminiproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText editTextUsername;
+    EditText editTextPassword;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        editTextUsername = findViewById(R.id.editTextUserNameLogin);
+        editTextPassword = findViewById(R.id.editTextUserPasswordLogin);
+
+        progressBar = findViewById(R.id.progressBar2);
+
         Objects.requireNonNull(getSupportActionBar()).hide();
 
+    }
+
+    public void login(View view)
+    {
+        String username = editTextUsername.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        if(username.isEmpty())
+        {
+            editTextUsername.setError("Please Enter Your Username.");
+            return;
+        }
+
+        if(password.isEmpty())
+        {
+            editTextPassword.setError("Please Enter Your Password.");
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e == null)
+                {
+                    showAlert("Login Successful", "Welcome, " + username + "!", false);
+                }
+                else
+                {
+                    ParseUser.logOut();
+                    showAlert("Login Fail", e.getMessage() + " Please try again", true);
+                }
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    private void showAlert(String title, String message, boolean error) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", (dialog, which) -> {
+                    dialog.cancel();
+                    if (!error) {
+//                        Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//                        startActivity(intent);
+                        Toast.makeText(LoginActivity.this,"Success",Toast.LENGTH_SHORT).show();
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
     }
 }
--- app/src/main/java/com/example/wechatminiproject/MainActivity.java
+++ app/src/main/java/com/example/wechatminiproject/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.wechatminiproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextUserEmail;
+    EditText editTextUserName;
+    EditText editTextUserPassword;
+    EditText editTextUserConfirm;
+
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        editTextUserName = findViewById(R.id.editTextUserName);
+        editTextUserEmail = findViewById(R.id.editTextUserEmail);
+        editTextUserPassword = findViewById(R.id.editTextUserPassword);
+        editTextUserConfirm = findViewById(R.id.editTextUserConfirmPassword);
+
+        progressBar = findViewById(R.id.progressBar);
 
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
@@ -27,5 +47,89 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void signUp(View view)
+    {
+        String email = editTextUserEmail.getText().toString();
+        String username = editTextUserName.getText().toString();
+        String password = editTextUserPassword.getText().toString();
+        String confirm = editTextUserConfirm.getText().toString();
+
+        if(email.isEmpty())
+        {
+            editTextUserEmail.setError("Please Enter Your Email ID.");
+            return;
+        }
+
+        if(!email.contains("@somaiya.edu"))
+        {
+            editTextUserEmail.setError("Please Enter Your Somaiya Email ID.");
+            return;
+        }
+
+        if(username.isEmpty())
+        {
+            editTextUserName.setError("Please Enter Your Username.");
+            return;
+        }
+
+        if(password.isEmpty())
+        {
+            editTextUserPassword.setError("Please Enter Your Password.");
+            return;
+        }
+
+        if(confirm.isEmpty())
+        {
+            editTextUserConfirm.setError("Please Enter Your Confirmation Password.");
+            return;
+        }
+
+        if(!password.equals(confirm))
+        {
+            editTextUserPassword.setError("Both Passwords Should Match.");
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e == null)
+                {
+                    ParseUser.logOut();
+                    progressBar.setVisibility(View.INVISIBLE);
+                    showAlert("Account Created Successfully!", "Please verify your email before Login", false);
+                }
+                else
+                {
+                    ParseUser.logOut();
+                    showAlert("Error Account Creation failed", "Account could not be created" + " :" + e.getMessage(), true);
+                }
+            }
+        });
+    }
+
+    private void showAlert(String title, String message, boolean error) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", (dialog, which) -> {
+                    dialog.cancel();
+                    if (!error) {
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                });
+        AlertDialog ok = builder.create();
+        ok.show();
     }
 }
